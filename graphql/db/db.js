0feafb46d35ce0ba3504224d220d7c315082ad46
SeanKim <sean.kim84@gmail.com>
2018-09-08 13:13:21 +0700

    Delete Mutation

--- graphql/db/db.js
+++ graphql/db/db.js
@@ -24,13 +24,13 @@ let movies = [
 export const getMovies = () => movies;
 
 export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    const filteredMovies = movies.filter(movie => movie.id === id);
     return filteredMovies[0];
 }
 
 export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
-    if(movies.length > cleandMovies.length ) {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanedMovies.length) {
         movies = cleanedMovies;
         return true;
     } else {
