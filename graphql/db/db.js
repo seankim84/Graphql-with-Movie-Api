fee096d5ef84874dabd8cc29f998e362e5b537fe
SeanKim <sean.kim84@gmail.com>
2018-09-09 19:40:11 +0700

    Final

--- graphql/db/db.js
+++ graphql/db/db.js
@@ -5,12 +5,12 @@ const LIST_MOVIES_URL = `${BASE_URL}list_movie.json`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
 const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getMovies = async (limt, rating) => {
+export const getMovies = async (limit, rating) => {
     const {
         data: {
-            data: {movies}
+            data: { movies }
         }
-} = await axios(LIST_MOVIES_URL, {
+} = await axios (LIST_MOVIES_URL, {
     params: {
         limit,
         minimum_rating: rating
@@ -24,7 +24,7 @@ export const getMovie = async id => {
         data: {
             data: { movie }
         }
-    } = await axios(MOVIE_DETAILS_URL, {
+    } = await axios (MOVIE_DETAILS_URL, {
         params: {
             movie_id: id
         }
@@ -37,7 +37,7 @@ export const getSuggestions = async id => {
         data: {
             data: { movies }
         }
-    } = await axios(MOVIE_SUGGESTIONS_URL, {
+    } = await axios (MOVIE_SUGGESTIONS_URL, {
         params: {
             movie_id: id
         }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,11 @@
-import { getMovies } from './db/db';
+import { getMovies, getMovie, getSuggestions } from './db/db';
 
 const resolvers = {
     Query: {
-        movies: (_, {rating, limit}) => getMovies(limit, rating) 
+        movies: (_, { rating, limit }) => getMovies(limit, rating),
+        movie: (_, { id }) => getMovie(id),
+        suggestions: (_, { id }) = getSuggestions(id)
     }
-}
+};
 
 export default resolvers;
